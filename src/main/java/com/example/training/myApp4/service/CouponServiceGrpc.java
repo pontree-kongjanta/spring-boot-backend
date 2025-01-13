8134d15a01eabe943404eab9ec52692b96c4e34f
soulxseeksgg <pontree.kongjanta@gmail.com>
2025-01-13 08:23:21 +0700

    using gprc net.devh grpc-client-spring-boot-starter

--- src/main/java/com/example/training/myApp4/service/CouponServiceGrpc.java
+++ src/main/java/com/example/training/myApp4/service/CouponServiceGrpc.java
@@ -3,6 +3,7 @@ package com.example.training.myApp4.service;
 import com.example.training.myApp4.response.CouponDiscountResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import net.devh.boot.grpc.example.GrpcCouponServiceGrpc;
 import net.devh.boot.grpc.example.GrpcCouponServiceRequest;
 import net.devh.boot.grpc.example.GrpcCouponServiceResponse;
@@ -10,13 +11,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CouponServiceGrpc {
+
+    @GrpcClient("couponServiceGrpc")
+    private GrpcCouponServiceGrpc.GrpcCouponServiceBlockingStub blockingStub;
+
     public CouponDiscountResponse getCouponDiscount(String couponCode, double originalPrice) {
-
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565)
-                .usePlaintext()
-                .build();
-
-        GrpcCouponServiceGrpc.GrpcCouponServiceBlockingStub blockingStub = GrpcCouponServiceGrpc.newBlockingStub(channel);
 
         GrpcCouponServiceRequest request = GrpcCouponServiceRequest.newBuilder()
                 .setCouponCode(couponCode)
